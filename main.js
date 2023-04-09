fbf13bd9e06aaf0786f2f6fe665ca51f76a314fd
Zach <Zach>
2023-04-10 02:20:55 +0800

    feat: modify code to search 10-K 10-Q and update their status

--- main.js
+++ main.js
@@ -5,8 +5,10 @@ import { GoogleSpreadsheet } from "google-spreadsheet";
 import dayjs from "dayjs";
 import { readFile } from "fs/promises";
 
+// sheet https://docs.google.com/spreadsheets/d/1l8Wtu7aLmfPMinx67Ja6EgJoKw8yELcStHyKTB8SCnY/edit#gid=1248092653
+// node main
 /**
- * 上傳發放狀態
+ * 取得 sheet data
  * @param {String} docID docID
  * @param {String} sheetID sheetID
  * @param {String} credentialsPath credentialsPath
@@ -47,14 +49,25 @@ async function save(row) {
 }
 
 /**
- * 找到 10-K 的 link
+ * 延遲 X 秒
+ * @param {Number} time time
+ */
+const delay = (time) => {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, time);
+  });
+};
+
+/**
+ * 找到 links
  * @param {Object} page page
  * @param {String} year year
+ * @param {String} keyword keyword
  */
-async function link10K(page, year) {
+async function getLinks(page, year, keyword) {
   try {
     const trs = await page.$$("#filingsTable tr");
-
+    let links = [];
     for (const tr of trs) {
       const tdFormType = await tr.$$("td.dtr-control");
       const tdFilingDate = await tr.$$("td.sorting_1");
@@ -66,14 +79,13 @@ async function link10K(page, year) {
           el.textContent.trim()
         );
 
-        if (formType === "10-K" && year === dayjs(filingDate).format("YYYY")) {
+        if (formType === keyword && year === dayjs(filingDate).format("YYYY")) {
           const href = await tr.$eval("a", (element) => element.href);
-
-          return href;
+          links.push(href);
         }
       }
     }
-    return null;
+    return links;
   } catch (error) {
     return null;
   }
@@ -81,24 +93,31 @@ async function link10K(page, year) {
 
 /**
  * 找到關鍵字
- * @param {Object} page page
+ * @param {Object} browser browser
+ * @param {Array} links links
  */
-async function findKeywords(page) {
+async function findKeywords(browser, links) {
   try {
+    const match = [];
     const keywords = JSON.parse(
       await readFile(new URL("./findData.json", import.meta.url))
     );
+    const newPage = await browser.newPage();
+    await newPage.setViewport({ width: 1080, height: 1024 });
 
-    const content = await page.content();
-    const match = [];
-    for (let i = 0; i < keywords.length; i++) {
-      const keyword = keywords[i];
-      const regex = new RegExp(keyword, "gi");
+    for (let i = 0; i < links.length; i++) {
+      await newPage.goto(links[i]);
 
-      if (content.match(regex)) {
-        match.push(i + 1);
+      const content = await newPage.content();
+      for (let j = 0; j < keywords.length; j++) {
+        const keyword = keywords[j];
+        const regex = new RegExp(keyword, "gi");
+        if (content.match(regex)) {
+          match.push(j + 1);
+        }
       }
     }
+    await newPage.close();
 
     return match;
   } catch (error) {
@@ -107,21 +126,12 @@ async function findKeywords(page) {
 }
 
 /**
- * 延遲 X 秒
- * @param {Number} time time
- */
-const delay = (time) => {
-  return new Promise(function (resolve) {
-    setTimeout(resolve, time);
-  });
-};
-
-/**
  * 操作 Dom 元素
  * @param {Object} page page
  * @param {String} year year
+ * @param {String} keyword keyword
  */
-const domOperate = async (page, year) => {
+const domOperate = async (page, year, keyword) => {
   try {
     // 確認 View Filings 是否要打開以及點擊
     const isBtnVisible = await page.evaluate(() => {
@@ -136,7 +146,7 @@ const domOperate = async (page, year) => {
     // 選擇表單類型為 10-K 的輸入框
     const formTypeSelect = await page.$('input[id="searchbox"]');
     await formTypeSelect.click({ clickCount: 3 });
-    await formTypeSelect.type("10-K");
+    await formTypeSelect.type(keyword);
     // await delay(500);
 
     // 選擇填報日期為 year 年的輸入框
@@ -166,46 +176,81 @@ const main = async () => {
   try {
     const sheet = await getSheet(
       "1l8Wtu7aLmfPMinx67Ja6EgJoKw8yELcStHyKTB8SCnY",
-      "1838907097"
+      "1248092653"
     );
     const rows = await sheet.getRows();
 
-    await sheet.loadCells(`A2:H${rows.length + 1}`);
+    const bar1 = new cliProgress.SingleBar(
+      {},
+      cliProgress.Presets.shades_classic
+    );
 
-    for (let i = 0; i < rows.length; i++) {
+    bar1.start(rows.length, 0);
+
+    for (let i = 90; i < rows.length; i++) {
       const row = rows[i];
       const rowData = row._rawData;
 
-      const secLink = sheet.getCell(i + 1, 6);
+      // const secLink = sheet.getCell(i + 1, 6);
       const year = dayjs(rowData[3]).format("YYYY");
-      const hyperlink = secLink?.hyperlink;
+      const CIKNumber = rowData[4];
+      const secLink = `https://www.sec.gov/edgar/browse/?CIK=${CIKNumber}`;
 
-      if (hyperlink) {
+      if (secLink) {
         const page = await browser.newPage();
-        await page.goto(hyperlink);
+        await page.goto(secLink);
         await page.setViewport({ width: 1080, height: 1024 });
-
         await delay(500);
-        await domOperate(page, year);
 
-        const link = await link10K(page, year);
-        console.log(link);
+        let link10KList = [];
+        let link10QList = [];
+        let result10K = [];
+        let result10Q = [];
 
-        if (link) {
-          // const linkPage = await browser.newPage();
-          await page.goto(link);
+        // 找 10-K
+        await domOperate(page, year, "10-K");
+        link10KList = await getLinks(page, year, "10-K");
+        if (link10KList.length) {
+          result10K = await findKeywords(browser, link10KList);
+        }
+        // console.log(chalk.bgBlue("link10KList", link10KList));
+        // console.log(chalk.bgCyan("result10K", result10K));
 
-          const result = await findKeywords(page);
-          console.log("result", result);
+        if (result10K.length) {
+          row["10-K"] = result10K.join("、");
+        } else if (link10KList.length === 0) {
+          row["10-K"] = "no file";
+        } else {
+          row["10-K"] = "no match";
         }
 
-        // 等待搜索結果頁面載入
+        if (result10K.length === 0) {
+          // 找 10-Q
+          await domOperate(page, year, "10-Q");
+          link10QList = await getLinks(page, year, "10-Q");
+          if (link10QList.length) {
+            result10Q = await findKeywords(browser, link10QList);
+          } else {
+            row["10-Q"] = "no file";
+          }
+          // console.log(chalk.bgBlueBright("link10QList", link10QList));
+          // console.log(chalk.bgGreenBright("result10Q", result10Q));
 
-        console.log(i, "end");
+          if (result10Q.length) {
+            row["10-Q"] = result10Q.join("、");
+          } else if (link10QList.length === 0) {
+            row["10-Q"] = "no file";
+          } else {
+            row["10-Q"] = "no match";
+          }
+        }
+        await save(row);
 
         await page.close();
       }
+      bar1.increment();
     }
+    bar1.stop();
   } catch (error) {
     console.log(error);
   } finally {
